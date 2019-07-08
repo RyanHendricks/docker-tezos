c6fafe50025d720ce4c17342e549805b581e26a9
Ryan <ryanhendricks@gmail.com>
2019-07-08 18:07:04 -0400

    Refactor entrypoint script

--- scripts/entrypoint.sh
+++ scripts/entrypoint.sh
@@ -3,13 +3,36 @@
 # exit script on any error
 set -e
 
-wget http://quicksync.tzdutch.com/mainnet-BMKkyoQRCUHh2nLxZE4o6q3ExQDeGntNsdMKTbzq6gEXRg6HguV.full.tar.lz4
-lz4 -d mainnet-BMKkyoQRCUHh2nLxZE4o6q3ExQDeGntNsdMKTbzq6gEXRg6HguV.full.tar.lz4 | tar xf -
 
-mkdir -p /root/.tezos-node
-cp /bootstrap/version.json /root/.tezos-node/version.json
-cp /bootstrap/identity.json /root/.tezos-node/identity.json
+if [ ! -d "/root/tezos-data" ]; then
 
-tezos-node snapshot import mainnet-BMKkyoQRCUHh2nLxZE4o6q3ExQDeGntNsdMKTbzq6gEXRg6HguV.full
+  echo "Creating directory for Tezos data"
+  mkdir -p /root/tezos-data
+ 
+  echo "Creating Tezos configuration"
+  tezos-node config init --data-dir=/root/tezos-data
+  tezos-node config update --data-dir=/root/tezos-data
 
+  echo "Bootstrapping Tezos Node"
+  mkdir -p /tmp/bootstrap
+  cd /tmp/bootstrap
+  wget https://storage.googleapis.com/node-bootstraps/tezos/mainnet.full.tar.lz4
+  lz4 -d -v --rm mainnet.full.tar.lz4 | tar xf -
+  tezos-node snapshot import mainnet.full
+
+  echo "Cleaning up..."
+  cd /root/tezos-data
+  rm -r /tmp/bootstrap
+  
+fi
+
+
+if [ ! -f "/root/tezos-data/identity.json" ]; then
+  echo "No identity file found, generating new identity for Tezos node"
+  tezos-node identity generate
+fi
+
+
+# exec tezos-node run --data-dir=/root/tezos-data --history-mode full --rpc-addr 0.0.0.0:8732 --cors-header='content-type' --cors-origin='*'
+echo "Starting Tezos Node via Supervisor Process Manager"
 exec supervisord --nodaemon --configuration /etc/supervisor/supervisord.conf
